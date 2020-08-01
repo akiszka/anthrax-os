161aa307a1446c1d612dd1e72475d1ea9a02d2ad
Antoni Kiszka <antonikiszka@icloud.com>
2020-08-01 22:51:42 +0200

    Bug fix. Style changes.
    The crt*.o files were not linked in the correct order. This could cause some strange issues in the future.

--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -10,7 +10,7 @@ void kernel_main(multiboot_info_t* mbt, unsigned int magic) {
   printf("Hello, kernel World!\n");
   multiboot_memory_map_t* entry = mbt->mmap_addr;
   while(entry < mbt->mmap_addr + mbt->mmap_length) {
-    simple_printf("mem entry: %llx, length %llx, type %x\n", entry->addr, entry->len, entry->type);
+    printf("mem entry: %llx, length %llx, type %x\n", entry->addr, entry->len, entry->type);
     entry = (multiboot_memory_map_t*) ((unsigned int) entry + entry->size + sizeof(entry->size));
   }
 }
